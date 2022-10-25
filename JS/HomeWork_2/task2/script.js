63d53ac63b37908e55908b3d0ed1ed6b1c97861f
Maryia Engel <maryiaengel@gmail.com>
2022-10-24 17:53:46 -0700

    HomeWork_2

---
+++ JS/HomeWork_2/task2/script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+let name = prompt("What is your name?");
+alert(`${name}`)
+result = confirm(`Your name, ${name}, correct?`);
---
+++ JS/HomeWork_2/task3/script.js
@@ -0,0 +1,12 @@
+'use strict';
+ 
+let number = 12.5;
+let num = 245;
+let message = "Hello";
+let str = undefined;
+let age = null;
+let fieldchecked = true;
+let iterable = [1,5,7,12,3,"значение"];
+let user = {'Вася':'Имя'};
+let FunctionDeclaration = function() {};
+alert(`${typeof number} ${typeof num} ${typeof message} ${typeof str} ${typeof age} ${typeof fieldchecked} ${typeof iterable} ${typeof user} ${typeof FunctionDeclaration}`);
