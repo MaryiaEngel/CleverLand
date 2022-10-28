94519ea58df8c692a13574d56ca409af92139031
Maryia Engel <maryiaengel@gmail.com>
2022-10-28 06:59:27 -0700

    HomeWork_3

--- JS/HomeWork_3/task3/script.js
+++ JS/HomeWork_3/task3/script.js
@@ -2,4 +2,4 @@
 var hour = prompt("Введите сколько сейчас часов?");
 var minute = prompt("Минут?");
 var sec = prompt("Секунд?");
-println(alert(hour+':'+minute+':'+sec));
+alert(hour+':'+minute+':'+sec);
