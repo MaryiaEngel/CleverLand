9d615aabc9a7c1f8d283c0a78eb88f6d9aa66e25
Maryia Engel <maryiaengel@gmail.com>
2022-11-01 05:51:46 -0700

    HomeWork_4

--- JS/HomeWork_4/task6/script.js
+++ JS/HomeWork_4/task6/script.js
@@ -4,7 +4,10 @@ let rideQuantity = prompt("How many rides are you planing to take this month?");
 rideQuantity = Number(rideQuantity);
 if(rideQuantity > 60){
     let ticketsSixty = Math.floor(rideQuantity / 60);
-    if((rideQuantity % 60) > 10){
+    if((rideQuantity % 60) == 0){
+        alert(`You gonna need ${ticketsSixty} ticket for 440 rubles `)
+    }
+    else if((rideQuantity % 60) > 10){
         let ticketsTen = Math.floor((rideQuantity % 60) / 10);
         let ticketOne = (((rideQuantity % 60) % 10) / 1);
         alert(`You gonna need ${ticketsSixty} ticket for 440 rubles, ${ticketsTen} tickets for 125 rubles and ${ticketOne} tickets for 15 rubles`)
