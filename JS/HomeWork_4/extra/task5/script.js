bd6d90748e0bd154adfb0035c4e89809b5922fbf
Maryia Engel <maryiaengel@gmail.com>
2022-11-02 16:00:56 -0700

    HomeWork_4

--- JS/HomeWork_4/extra/task5/script.js
+++ JS/HomeWork_4/extra/task5/script.js
@@ -1,117 +1,131 @@
 'use strict';
-
 let question = prompt("Введите число от 100 до 999");
-switch(Math.floor(question / 100)){
+let hundreds;
+let subDozens;
+let dozens;
+let units;
+switch (Math.floor(question / 100)) {
     case 1:
-        console.log('Сто ');
+        hundreds = 'Сто';
         break
     case 2:
-        console.log('Двести ');
+        hundreds = 'Двести ';
         break
     case 3:
-        console.log('Триста ');
+        hundreds = 'Триста ';
         break
     case 4:
-        console.log('Четыреста ');
+        hundreds = 'Четыреста ';
         break
     case 5:
-        console.log('Пятьсот ');
+        hundreds = 'Пятьсот ';
         break
     case 6:
-        console.log('Шестьсот ');
+        hundreds = 'Шестьсот ';
         break
     case 7:
-        console.log('Семьсот ');
+        hundreds = 'Семьсот ';
         break
     case 8:
-        console.log('Восемьсот ');
+        hundreds = 'Восемьсот ';
         break
     case 9:
-        console.log('Девятьсот ');
-        break}
-        switch(question%100){
-            case 10:
-                console.log('десять ');
-                break
-            case 11:
-                console.log('одиннадцать');
-                break
-            case 12:
-                console.log('двенадцать ');
-                break
-            case 13:
-                console.log('тринадцать ');
-                break
-            case 14:
-                console.log('четырнадцать');
-                break
-            case 15:
-                console.log('пятнадцать ');
-                break
-            case 16:
-                console.log('шестнадцать ');
-                break
-            case 17:
-                console.log('семнадцать ');
-                break
-            case 18:
-                console.log('восемнадцать ');
-                break
-            case 19:
-                console.log('девятнадцать ');
-                break
-            }
-            switch(Math.floor(question%100)){
-                case 2:
-                    console.log('двадцать ');
-                    break
-                case 3:
-                    console.log('тридцать ');
-                    break
-                case 4:
-                    console.log('сорок ');
-                    break
-                case 5:
-                    console.log('пятьдесят ');
-                    break
-                case 6:
-                    console.log('шестьдесят ');
-                    break
-                case 7:
-                    console.log('семьдесят ');
-                    break
-                case 8:
-                    console.log('восемьдесят ');
-                    break
-                case 9:
-                    console.log('девяносто ');
-                    break
-                }
-                    switch((question%100)%10){
-                        case 1:
-                            console.log('один');
-                            break
-                        case 2:
-                            console.log('два');
-                            break
-                        case 3:
-                            console.log('три');
-                            break
-                        case 4:
-                            console.log('четыре');
-                            break
-                        case 5:
-                            console.log('пять');
-                            break
-                        case 6:
-                            console.log('шесть');
-                            break
-                        case 7:
-                            console.log('семь');
-                            break
-                        case 8:
-                            console.log('восемь');
-                            break
-                        case 9:
-                            console.log('девять');
-                            break}
+        hundreds = 'Девятьсот ';
+        break
+}
+
+switch (question % 100) {
+    case 10:
+        subDozens = 'десять ';
+        break
+    case 11:
+        subDozens = 'одиннадцать';
+        break
+    case 12:
+        subDozens = 'двенадцать ';
+        break
+    case 13:
+        subDozens = 'тринадцать ';
+        break
+    case 14:
+        subDozens = 'четырнадцать';
+        break
+    case 15:
+        subDozens = 'пятнадцать ';
+        break
+    case 16:
+        subDozens = 'шестнадцать ';
+        break
+    case 17:
+        subDozens = 'семнадцать ';
+        break
+    case 18:
+        subDozens = 'восемнадцать ';
+        break
+    case 19:
+        subDozens = 'девятнадцать ';
+        break
+    default:
+        subDozens = '';
+}
+switch ((question % 100) - ((question % 100) % 10)) {
+    case 20:
+        dozens = 'двадцать ';
+        break
+    case 30:
+        dozens = 'тридцать ';
+        break
+    case 40:
+        dozens = 'сорок ';
+        break
+    case 50:
+        dozens = 'пятьдесят ';
+        break
+    case 60:
+        dozens = 'шестьдесят ';
+        break
+    case 70:
+        dozens = 'семьдесят ';
+        break
+    case 80:
+        dozens = 'восемьдесят ';
+        break
+    case 90:
+        dozens = 'девяносто ';
+        break
+    default:
+        dozens = '';
+}
+switch ((question % 100) % 10) {
+    case 1:
+        units = 'один';
+        break
+    case 2:
+        units = 'два';
+        break
+    case 3:
+        units = 'три';
+        break
+    case 4:
+        units = 'четыре';
+        break
+    case 5:
+        units = 'пять';
+        break
+    case 6:
+        units = 'шесть';
+        break
+    case 7:
+        units = 'семь';
+        break
+    case 8:
+        units = 'восемь';
+        break
+    case 9:
+        units = 'девять';
+        break
+    default:
+        units = '';
+}
+
+alert(`${hundreds} ${dozens || subDozens} ${subDozens ? '' : units}`)
