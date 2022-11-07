08d121f3cc590ffb3e85bba15bfa14cc2ea4b49a
Maryia Engel <maryiaengel@gmail.com>
2022-11-07 05:39:31 -0800

    HomeWork_5

--- JS/HomeWork_5/extra/task3/script.js
+++ JS/HomeWork_5/extra/task3/script.js
@@ -10,15 +10,15 @@ while(n%1 == 0){
     else if(n >= 10 && n<= 99){
         console.log("Вы ввели двухзначное число, для него результат:");
         console.log(`${Math.floor(n/10)} , ${(n%10)}`);
-        console.log(Math.floor(n/10) + (n%10))
+        console.log(Math.floor(n/10) + (n%10));
         console.log(`${(n%10)}${Math.floor(n/10)}`)}
     else if(n >= 100 && n<= 999 ){
-        console.log("Вы ввели трёхзначное число, для него результат:")
+        console.log("Вы ввели трёхзначное число, для него результат:");
         console.log(`${Math.floor(n/100)}, ${Math.floor((n%100)/10)}, ${(n%10)}`);
         console.log(Math.floor(n/100) + Math.floor((n%100)/10)+ (n%10));
         console.log(`${(n%10)}${Math.floor((n%100)/10)}${Math.floor(n/100)}`)}
     else if(n >= 1000 && n<= 9999){
-        console.log("Вы ввели четырёхзначное число, для него результат :")
+        console.log("Вы ввели четырёхзначное число, для него результат :");
         console.log(`${Math.floor(n/1000)}, ${Math.floor((n%1000)/100)}, ${Math.floor((n%100)/10)}, ${(n%10)}`);
         console.log(Math.floor(n/1000) + Math.floor((n%1000)/100) + Math.floor((n%100)/10) + (n%10));
         console.log(`${(n%10)}${Math.floor((n%100)/10)}${Math.floor((n%1000)/100)}${Math.floor(n/1000)}`)}
