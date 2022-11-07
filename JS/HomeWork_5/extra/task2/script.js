231158ca0abfa71bc006d80b20c8b0f1464229a1
Maryia Engel <maryiaengel@gmail.com>
2022-11-06 18:36:31 -0800

    HomeWork_5

--- JS/HomeWork_5/extra/task2/script.js
+++ JS/HomeWork_5/extra/task2/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let n = prompt('Введите любое число', '');
-for(let i = n-1 ; i >= 1 ; i--){
+for(let i = n-1 ; i >= 2 ; i--){
    n *= i;
     console.log(n);
 }
---
+++ JS/HomeWork_5/extra/task3/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let n = prompt('Введите любое число', '');
+while(n%1 == 0){
+    if(n >=1 && n<=10){
+        console.log(n);
+        console.log("1");
+        console.log(n)
+        console.log(n)}
+    else if(n >= 10 && n<= 99){
+        console.log(`${Math.floor(n/10)} , ${(n%10)}`);
+        console.log("2");
+        console.log(Math.floor(n/10) + (n%10))
+        console.log(`${(n%10)}${Math.floor(n/10)}`)}
+    else if(n >= 100 && n<= 999 ){
+        console.log(`${Math.floor(n/100)}, ${Math.floor((n%100)/10)}, ${(n%10)}`);
+        console.log("3")
+        console.log(Math.floor(n/100) + Math.floor((n%100)/10)+ (n%10));
+        console.log(`${(n%10)}${Math.floor((n%100)/10)}${Math.floor(n/100)}`)}
+    else if(n >= 1000 && n<= 9999){
+        console.log(`${Math.floor(n/1000)}, ${Math.floor((n%1000)/100)}, ${Math.floor((n%100)/10)}, ${(n%10)}`);
+        console.log("4")
+        console.log(Math.floor(n/1000) + Math.floor((n%1000)/100) + Math.floor((n%100)/10) + (n%10));
+        console.log(`${(n%10)}${Math.floor((n%100)/10)}${Math.floor((n%1000)/100)}${Math.floor(n/1000)}`)}
+        break}
