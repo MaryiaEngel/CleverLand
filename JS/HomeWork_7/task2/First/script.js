3c28ac05dcc635b363fbe59704415a3871c79c08
Maryia Engel <maryiaengel@gmail.com>
2022-11-12 10:51:48 -0800

    HomeWork_7

--- JS/HomeWork_7/task2/First/script.js
+++ JS/HomeWork_7/task2/First/script.js
@@ -2,7 +2,7 @@
 
 function sumNumbers(){
     if(a%1==0 && b%1==0){
-        sum =(b-a)*((b-a+1)/2);
+        let sum =((a+b)*((b-a)+1))/2;
         return( 'Сумма чисел равна  ' + sum );}
     else{
         return('Error!Введите натуральное число!')}}
--- JS/HomeWork_7/task2/Second/script.js
+++ JS/HomeWork_7/task2/Second/script.js
@@ -5,7 +5,7 @@ let b = prompt("Введите число");
 let sum;
 if(a%1==0 && b%1==0){
     sum = function(){
-        console.log('Сумма чисел равна  ' + (b-a)*((b-a+1)/2))}}
+        console.log('Сумма чисел равна  ' + (((a+b)*((b-a)+1))/2));}}
 else{
     console.log('Error!Введите натуральное число!')}
 sum();
