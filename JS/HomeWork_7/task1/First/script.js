d1e44ad402933d59fae8021a5a7eb0113eb3b3aa
Maryia Engel <maryiaengel@gmail.com>
2022-11-12 11:19:31 -0800

    HomeWork_7

--- JS/HomeWork_7/task1/First/script.js
+++ JS/HomeWork_7/task1/First/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-function calcDegree(r) {
+function calcDegree() {
     if(r>= 0 && r <=360){
       return r = (r * 3.14)/180;}
     else{
       return("Error, Wrong Input")}}
-r = prompt("Введите значение угла в градусах");
-console.log(calcDegree(r));
+let r = prompt("Введите значение угла в градусах");
+console.log(calcDegree());
 
 
