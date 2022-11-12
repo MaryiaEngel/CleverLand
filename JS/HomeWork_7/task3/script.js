61f393e0c0be456c7e93d1cb6475364beafc94e5
Maryia Engel <maryiaengel@gmail.com>
2022-11-11 18:07:13 -0800

    HomeWork_7

--- JS/HomeWork_7/task3/script.js
+++ JS/HomeWork_7/task3/script.js
@@ -7,3 +7,23 @@ function calcNumbers(n){
 let n = prompt('Введите любое число', '');
 console.log(calcNumbers(n));
 
+
+
+'use strict';
+
+//let n = prompt('Введите любое число', '');
+let calc = function (){
+    for(let i = n-1 ; i >= 2 ; i--)
+        n *=i;
+        console.log(n);}
+//calc();
+
+
+'use strict';
+
+//let n = prompt('Введите любое число', '');
+//let calc = () => {
+    for(let i = n-1 ; i >= 2 ; i--)
+        n *=i;
+        return n;}
+//console.log(calc());
