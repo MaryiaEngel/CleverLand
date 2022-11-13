8341b26359e361af3ef15dd4aa7813e4a8505f8d
Maryia Engel <maryiaengel@gmail.com>
2022-11-12 16:13:59 -0800

    HomeWork_7

--- JS/HomeWork_7/task2/Second/script.js
+++ JS/HomeWork_7/task2/Second/script.js
@@ -1,4 +1,20 @@
+'use strict';
 
+let sum=0;
+const a = +prompt("Введите число");
+const b = +prompt("Введите число");
+sumNumbers = function (){
+    console.log( 'Сумма чисел равна  ' + sum );}
+if(a%1==0 && b%1==0){
+    if (a < b) {
+        for (let i = a; i<=b; i++ ) {
+            sum += i;}
+    } else if (a>b) {
+        for (let i = a; i>=b; i-- ) {
+            sum += i;}}
+else{
+    console.log('Error!Введите натуральное число!')}}
+sumNumbers();
 
 
 
--- JS/HomeWork_7/task2/Third/script.js
+++ JS/HomeWork_7/task2/Third/script.js
@@ -1,3 +1,18 @@
+'use strict';
 
+let sum=0;
+const a = +prompt("Введите число");
+const b = +prompt("Введите число");
+const sumNumbers = () =>  'Сумма чисел равна  ' + sum ;
+if(a%1==0 && b%1==0){
+    if (a < b) {
+        for (let i = a; i<=b; i++ ) {
+            sum += i;}
+    } else if (a>b) {
+        for (let i = a; i>=b; i-- ) {
+            sum += i;}}
+else{
+    console.log('Error!Введите натуральное число!')}}
+console.log(sumNumbers());
 
 
