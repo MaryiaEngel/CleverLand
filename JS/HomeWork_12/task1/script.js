0679e397202e6091f09f4b35202c43b0d7a60125
Maryia Engel <maryiaengel@gmail.com>
2022-12-02 09:21:42 -0800

    HomeWork_12

--- JS/HomeWork_12/task1/script.js
+++ JS/HomeWork_12/task1/script.js
@@ -1,2 +1,7 @@
 'use strict';
 
+const first = document.getElementById("red");
+for(let i = 0; i < first.rows.length;i++){
+    first.rows[i].cells[i].style.backgroundColor = 'red';
+    first.rows[i].cells[first.rows.length-1-i].style.backgroundColor = 'red';
+}
