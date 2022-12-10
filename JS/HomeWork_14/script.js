9481c7fb1c5f132453705d4dfb22a654ff30ebb0
Maryia Engel <maryiaengel@gmail.com>
2022-12-10 10:50:31 -0800

    HomeWork_14

--- JS/HomeWork_14/script.js
+++ JS/HomeWork_14/script.js
@@ -1,24 +1,31 @@
-let timer;
-const r = document.getElementById('result');
-const btn = document.querySelector('.btn');
-btn.addEventListener('click', () =>
-{
-    btn.innerHTML = 'Пауза';
-    let a = Number(document.querySelector('#a').value);
-    let b = Number(document.querySelector('#b').value);   
-    
-     if (timer)
-    {
-      clearInterval(timer);
-    }
-if(a < b){
-  timer = setInterval(() => {
-    r.innerHTML = parseInt(a++);}, 1000)}
-else{
-    r.innerHTML = "Error";
+'use strict';
 
-}
-});
+const r = document.getElementById("result");
+const btn = document.querySelector(".btn");
+let timer;
+let value;
+let firstClick = true;
+
+btn.onclick = () => {
+  if(firstClick) {
+    let a = Number(document.querySelector('#a').value);
+    var b = Number(document.querySelector('#b').value); 
+    value = a;
+    firstClick = false;
+  }
+    if (btn.innerHTML === "Пуск") {
+        timer = setInterval(() => {
+          if(value == b) {
+            clearTimeout(timer);
+            btn.innerHTML = "Пуск";
+          }
+          r.innerHTML = parseInt(value++);}, 1000);
+    btn.innerHTML = "Пауза";
+  }
+    else {
+        clearTimeout(timer);
+        btn.innerHTML = "Пуск";
+    }}
 
 
 
